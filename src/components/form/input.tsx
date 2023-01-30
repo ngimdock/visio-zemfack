e11735ad0b9ffd2efe281ee6eed38d60a850c4ef
ngimdock <ngimdockzemfack@gmail.com>
2023-01-30 14:40:48 +0100

    Terminaison de la section Transcription

---
+++ src/components/form/input.tsx
@@ -0,0 +1,30 @@
+import { IconAsImage } from "../icon-as-image/icon-as-image";
+
+type InputType = {
+  value?: string;
+  placeholder?: string;
+  type: "number" | "text" | "search";
+  name?: string;
+  icon?: string;
+};
+export const Input = ({ type, icon, name, placeholder, value }: InputType) => {
+  return (
+    <div className="box-border flex items-center pl-2 space-x-1 border rounded-md border-custom-indigo/10">
+      {icon && <IconAsImage alt={name ?? "icon"} image={icon} />}
+      <input
+        type="text"
+        placeholder={placeholder}
+        name={name}
+        className=" placeholder:-translate-y-[2px] placeholder:text-xx py-1 text-custom-gray pl-1 placeholder:text-custom-gray bg-primary  focus:outline-none focus:border-0  caret-custom-gray"
+      />
+    </div>
+  );
+};
+
+const defaultProps: InputType = {
+  type: "text",
+  placeholder: "Entrer du text",
+  value: "",
+};
+
+Input.defaultvalue = defaultProps;
--- src/screens/life-meeting/modules/note-transcription/sections/transcription.tsx
+++ src/screens/life-meeting/modules/note-transcription/sections/transcription.tsx
@@ -1,3 +1,44 @@
+import { Badge } from "@/components/badge/badge";
+import { Input } from "@/components/form/input";
+import { P } from "@/components/p/p";
+
 export const Transcription = () => {
-  return <div>Note transcription Transcription</div>;
+  return (
+    <section>
+      <Input type="text" icon="search.svg" placeholder="Search..." />
+
+      <div className="mt-6 md:mt-4">
+        {[1, 2, 3].map((elt: number, index: number) => {
+          return (
+            <div
+              key={index}
+              className="mt-4 space-y-5 md:mt-2.5 md:space-y-3.5"
+            >
+              <div>
+                <Badge background="pink" classe="float-left mr-2">
+                  Tachel
+                </Badge>
+                <P color="custom-white" classe="max-w-lg">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                  quibusdam porro minima incidunt sit, reiciendis odio quas
+                  dolore
+                </P>
+              </div>
+
+              <div>
+                <Badge background="bleu" classe="float-left mr-2">
+                  Paul Alexis Dray
+                </Badge>
+                <P color="custom-white" classe="max-w-lg">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                  quibusdam porro minima incidunt sit, reiciendis odio quas
+                  dolore
+                </P>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
